38fd3614c8ef45f253dc87709c0718489ecf0567
Ridho Muhammad <5mblackburn7@gmail.com>
2021-12-08 09:25:05 +0700

    create server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const midTransClient = require('midtrans-client');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config();
+
+const SERVER_KEY = process.env.SERVER_KEY;
+const CLIENT_KEY = process.env.CLIENT_KEY;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, './src/public')))
+
+/**
+ * ===============
+ * Using Snap API
+ * ===============
+ */
+
+
+
+
+app.listen(3000,()=>{
+    console.log(`Server listening at port ${3000}`)
+})
